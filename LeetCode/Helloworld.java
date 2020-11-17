b2cd4965450bf720d023bee43854c02813ff19dd
pydi sai suraj <pydisaisuraj@pydis-MacBook-Pro.local>
2020-11-17 10:25:29 +0530

    java helloworld commit

---
+++ LeetCode/Helloworld.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Helloworld{
+    public static void main(String args[]){
+        System.out.println("HELLO WORLD");
+        System.out.println(2*2);
+    }
+}
--- Sample.md
+++ Sample.md
@@ -1,2 +1,3 @@
 Hello world 
 ready to push git
+ 
